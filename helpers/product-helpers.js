37c70ccdc14211a8fe49f6b2acbe09c2e9805b21
Abdullah muhammed <abdullaahmohammed3000@gmail.com>
2024-10-24 12:21:35 -0700

    all basic files added

---
+++ helpers/product-helpers.js
@@ -0,0 +1,10 @@
+var db=require('../Config/connection')
+
+module.exports={
+    addProduct:(product,callback)=>{
+        db.get().collection('product').insertOne(product).then((data)=>{
+            
+            callback(data.insertedId)
+        })
+    }
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var productHelper=require('../helpers/product-helpers')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -32,5 +33,23 @@ router.get('/', function(req, res, next) {
 
   res.render('admin/view-products',{admin:true,products})
 });
+router.get('/add-products',function(req,res){
+  res.render('admin/add-products')
+})
+router.post('/add-products',(req,res)=>{
 
+
+  productHelper.addProduct(req.body,(id)=>{
+    let image=req.files.image;
+    console.log(id);
+    image.mv('./public/product-images/'+id+'.jpg',(err,done)=>{
+      if(!err){
+        res.render("admin/add-products")
+      }else{
+        console.log(err);
+      }
+    })
+  })
+  
+})
 module.exports = router;
