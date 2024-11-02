4ba3daf768a1d1dcf5a0e8057938006d5c05b052
Abdullah muhammed <abdullaahmohammed3000@gmail.com>
2024-11-02 11:39:49 -0700

    user signup added with bcrypt and session

---
+++ helpers/user-helpers.js
@@ -0,0 +1,42 @@
+var db=require('../Config/connection')
+var collection=require('../Config/collections')
+const bcrypt=require('bcrypt')
+const { reject, resolve } = require('promise')
+
+
+
+module.exports={
+    doSignup:(userData)=>{
+        return new Promise(async(resolve,_reject)=>{
+            userData.Password=await bcrypt.hash(userData.Password,10)
+            db.get().collection(collection.USER_COLLECTION).insertOne(userData).then((data)=>{
+                resolve(data.ops[0])
+            })
+        })
+       
+    },
+    doLogin:(userData)=>{
+        return new Promise(async(resolve,reject)=>{
+            let loginStatus=false
+            let response={}
+            let user=await db.get().collection(collection.USER_COLLECTION).findOne({Email:userData.Email})
+            if(user){
+                bcrypt.compare(userData.Password,user.Password).then((status)=>{
+                    if(status){
+                        console.log("login success");
+                        response.user=user
+                        response.status=true    
+                        resolve(response)
+                    }else{
+                        console.log("Try Again");
+                        resolve({status:false})
+                    }
+                })
+            }else{
+                console.log('login failed')
+                resolve({status:false})
+            }
+        })
+    }
+
+}
